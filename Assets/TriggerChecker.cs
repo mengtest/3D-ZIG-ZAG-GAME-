2991d709cb6bce5ba9dde2731311f40cc3fbd760
Arma <armagnidavide@gmail.com>
2017-02-15 22:50:17 +0100

    Platform falls down after Ball goes away

---
+++ Assets/TriggerChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Ball")//if the object who exits from the collider has a tag=="Ball"
+        {
+            Invoke("FallDown", 0.5f);  //invoke the function FallDown() after 0.5sec      
+        }          
+    }
+    void FallDown() {
+        GetComponentInParent<Rigidbody>().useGravity=true;
+        Destroy(transform.parent.gameObject, 2f);
+    }
+}
