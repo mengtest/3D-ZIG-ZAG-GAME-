1a1d5ecbad5ae8ef97a682f3a35e70b7cfe2439f
Arma <armagnidavide@gmail.com>
2017-02-16 22:55:19 +0100

    Creating Diamonds and Randomly Spawning Them

--- Assets/PlatformSpawner.cs
+++ Assets/PlatformSpawner.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//this is a gameObject
+
 public class PlatformSpawner : MonoBehaviour {
 
     public GameObject platform;//the platform we want to spawn
+    public GameObject diamond;//the diamond we want to instantiate
+
     Vector3 lastPos;//the last position where our platform was
+
     float size; //the size of our platform.We need the size to colculate the new position.
+
     public bool gameOver;
+
     // Use this for initialization
 	void Start () {
         lastPos = platform.transform.position;
@@ -39,11 +44,22 @@ public class PlatformSpawner : MonoBehaviour {
         pos.x += size;
         lastPos = pos;
         Instantiate(platform, pos, Quaternion.identity);//instantiate the platform in the pos position with no rotation
+        int rand = Random.Range(0,4);
+        Vector3 posDiamond = new Vector3(pos.x, pos.y + 1, pos.z);
+        if (rand < 1) {
+            Instantiate(diamond, posDiamond, diamond.transform.rotation);
+        }
     }
     void SpawnZ() {
         Vector3 pos = lastPos;
         pos.z += size;
         lastPos = pos;
         Instantiate(platform, pos, Quaternion.identity);//instantiate the platform in the pos position with no rotation
+        int rand = Random.Range(0, 4);
+        Vector3 posDiamond = new Vector3(pos.x, pos.y + 1, pos.z);
+        if (rand < 1)
+        {
+            Instantiate(diamond, posDiamond, diamond.transform.rotation);
+        }
     }
 }
