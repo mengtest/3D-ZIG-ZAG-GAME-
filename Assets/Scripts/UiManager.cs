efd245260e92e853474725a24d235be2dba4ba5f
Arma <armagnidavide@gmail.com>
2017-03-07 23:01:37 +0100

    Scripting the UI Manager

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour {
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        zigzagPanel.GetComponent<Animator>().Play("panelUp");
+    }
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Reset() {
+        SceneManager.LoadScene(0);
+            }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
